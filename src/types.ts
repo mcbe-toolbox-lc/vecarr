284c0b41bdf9234306b0c5d85b7bb59e936202ea
LuckedCoronet <lc45@lc-studios.net>
2025-11-09 08:57:00 +0900

    feat: add types.ts

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Vector2Object = {
+	x: number;
+	y: number;
+};
+
+export type Vector2Array = [number, number];
+
+export type Vector3Object = {
+	x: number;
+	y: number;
+	z: number;
+};
+
+export type Vector3Array = [number, number, number];
