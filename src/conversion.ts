3e96fec7c46a19e9f6d0f079602778545b85f54c
LuckedCoronet <lc45@lc-studios.net>
2025-11-09 09:22:07 +0900

    docs: add jsdoc for members of conversion.ts

--- src/conversion.ts
+++ src/conversion.ts
@@ -1,5 +1,13 @@
 import type { Vector2Array, Vector2Object, Vector3Array, Vector3Object } from "./types";
 
+/**
+ * Converts a 2D vector object `{x, y}` to a `[x, y]` array.
+ * Uses fallback values or 0 if properties are missing.
+ *
+ * @param obj - The source object.
+ * @param fallbackValues - Fallback values.
+ * @returns The resulting `[x, y]` array.
+ */
 export const toArr2 = (
 	obj?: Partial<Vector2Object>,
 	fallbackValues?: Partial<Vector2Object>,
@@ -9,11 +17,25 @@ export const toArr2 = (
 	return [x, y];
 };
 
+/**
+ * Converts a 2D vector array `[x, y]` to an `{x, y}` object.
+ *
+ * @param arr - The source array.
+ * @returns The resulting `{x, y}` object.
+ */
 export const toObj2 = (arr: Vector2Array): Vector2Object => ({
 	x: arr[0],
 	y: arr[1],
 });
 
+/**
+ * Converts a 3D vector object `{x, y, z}` to a `[x, y, z]` array.
+ * Uses fallback values or 0 if properties are missing.
+ *
+ * @param obj - The source object.
+ * @param fallbackValues - Fallback values.
+ * @returns The resulting `[x, y, z]` array.
+ */
 export const toArr3 = (
 	obj?: Partial<Vector3Object>,
 	fallbackValues?: Partial<Vector3Object>,
@@ -24,6 +46,12 @@ export const toArr3 = (
 	return [x, y, z];
 };
 
+/**
+ * Converts a 3D vector array `[x, y, z]` to an `{x, y, z}` object.
+ *
+ * @param arr - The source array.
+ * @returns The resulting `{x, y, z}` object.
+ */
 export const toObj3 = (arr: Vector3Array): Vector3Object => ({
 	x: arr[0],
 	y: arr[1],
