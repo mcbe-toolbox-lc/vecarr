5cc3d5b3d9249c02f4d43620a3819b0857f63cf8
LuckedCoronet <lc45@lc-studios.net>
2025-11-09 10:55:33 +0900

    feat: add HybridVec3

---
+++ src/hybrid.ts
@@ -0,0 +1,85 @@
+import * as conversion from "./conversion";
+import type { Vector3Array, Vector3Object } from "./types";
+
+export class HybridVec3 {
+	#array: Vector3Array;
+
+	constructor(
+		vec?: Vector3Array | Partial<Vector3Object>,
+		fallbackValues?: Partial<Vector3Object>,
+	) {
+		if (Array.isArray(vec)) {
+			this.#array = vec;
+		} else {
+			this.#array = conversion.toArr3(vec, fallbackValues);
+		}
+
+		const xDesc = Object.getOwnPropertyDescriptor(HybridVec3.prototype, "x")!;
+		const yDesc = Object.getOwnPropertyDescriptor(HybridVec3.prototype, "y")!;
+		const zDesc = Object.getOwnPropertyDescriptor(HybridVec3.prototype, "z")!;
+
+		Object.defineProperty(this, "x", { enumerable: true, get: xDesc.get, set: xDesc.set });
+		Object.defineProperty(this, "y", { enumerable: true, get: yDesc.get, set: yDesc.set });
+		Object.defineProperty(this, "z", { enumerable: true, get: zDesc.get, set: zDesc.set });
+
+		// Define 'length' as a non-enumerable property
+		Object.defineProperty(this, "length", {
+			value: 3,
+			writable: false,
+			enumerable: false,
+			configurable: false,
+		});
+	}
+
+	// Object support
+
+	get x(): number {
+		return this.#array[0];
+	}
+	set x(value: number) {
+		this.#array[0] = value;
+	}
+	get y(): number {
+		return this.#array[1];
+	}
+	set y(value: number) {
+		this.#array[1] = value;
+	}
+	get z(): number {
+		return this.#array[2];
+	}
+	set z(value: number) {
+		this.#array[2] = value;
+	}
+
+	// Array support
+
+	readonly length = 3;
+
+	[index: number]: number;
+
+	get [0](): number {
+		return this.#array[0];
+	}
+	set [0](value: number) {
+		this.#array[0] = value;
+	}
+	get [1](): number {
+		return this.#array[1];
+	}
+	set [1](value: number) {
+		this.#array[1] = value;
+	}
+	get [2](): number {
+		return this.#array[2];
+	}
+	set [2](value: number) {
+		this.#array[2] = value;
+	}
+
+	*[Symbol.iterator](): Iterator<number> {
+		yield this.#array[0];
+		yield this.#array[1];
+		yield this.#array[2];
+	}
+}
