f7cbd8bf4633462fdffaaa56790d54bf777a0bf3
LuckedCoronet <lc45@lc-studios.net>
2025-11-09 11:01:58 +0900

    test: write tests for HybridVec2

--- src/hybrid.test.ts
+++ src/hybrid.test.ts
@@ -1,5 +1,45 @@
 import { describe, expect, it } from "vitest";
-import { HybridVec3 } from "./hybrid";
+import { HybridVec2, HybridVec3 } from "./hybrid";
+
+describe("HybridVec2", () => {
+	it("supports indexed access", () => {
+		const hybridVec = new HybridVec2([6, 9]);
+
+		expect(hybridVec[0]).toBe(6);
+		expect(hybridVec[1]).toBe(9);
+		expect(hybridVec[2]).toBeUndefined(); // Out of bounds
+	});
+
+	it("supports object property access", () => {
+		const hybridVec = new HybridVec2([6, 9]);
+
+		expect(hybridVec.x).toBe(6);
+		expect(hybridVec.y).toBe(9);
+	});
+
+	it("supports for...of loop", () => {
+		const hybridVec = new HybridVec2([6, 9]);
+
+		let values: number[] = [];
+		for (const value of hybridVec) {
+			values.push(value);
+		}
+
+		expect(values).toStrictEqual([6, 9]);
+	});
+
+	it("supports array spread syntax", () => {
+		const hybridVec = new HybridVec2([6, 9]);
+		const spreaded = [...hybridVec];
+		expect(spreaded).toStrictEqual([6, 9]);
+	});
+
+	it("supports object spread syntax", () => {
+		const hybridVec = new HybridVec2([6, 9]);
+		const spreaded = { ...hybridVec };
+		expect(spreaded).toStrictEqual({ x: 6, y: 9 });
+	});
+});
 
 describe("HybridVec3", () => {
 	it("supports indexed access", () => {
